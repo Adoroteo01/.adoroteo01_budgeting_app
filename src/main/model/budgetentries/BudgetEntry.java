640326763023090e8d60103c060f1dcce603f5ee
Adoroteo01 <andreitot13@gmail.com>
2024-10-05 22:43:09 -0700

    added contructor specifiction and setters + getters in BudgetEntry class

--- src/main/model/budgetentries/BudgetEntry.java
+++ src/main/model/budgetentries/BudgetEntry.java
@@ -2,5 +2,38 @@ package model.budgetentries;
 
 // A budget entry that has a name, budgeted amount, and actual amount
 public class BudgetEntry {
+    String name;
+    float budgetAmount;
+    float actualAmount;
+
+    // EFFECTS: creates a new BudgetEntry object with given name and budgetAmount
+    // the actualAmount starts at 0
+    public BudgetEntry(String name, float budgetAmount) {
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public float getBudgetAmount() {
+        return budgetAmount;
+    }
+
+    public float getActualAmount() {
+        return actualAmount;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setBudgetAmount(float budgetAmount) {
+        this.budgetAmount = budgetAmount;
+    }
+
+    public void setActualAmount(float actualAmount) {
+        this.actualAmount = actualAmount;
+    }
 
 }
