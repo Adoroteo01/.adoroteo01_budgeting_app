e2ffabdc87518c16d8d11a8c285d6fe5db21ae1e
Adoroteo01 <andreitot13@gmail.com>
2024-10-10 16:25:44 -0700

    updated Tracker Specification

--- src/main/model/Tracker.java
+++ src/main/model/Tracker.java
@@ -1,7 +1,21 @@
 package model;
 
+import java.util.List;
+
 // A spending tracker that holds TrackerEntrys for it's
 // corrisponding Budgeter
 public class Tracker {
+
+    List<TrackerEntry> entries;
+
+    // creates a new Tracker with no entries in list
+    public Tracker() {
+
+    }
+
+    // add entry to entries. 
+    protected void addEntry(TrackerEntry entry) {
+
+    }
 
 }
