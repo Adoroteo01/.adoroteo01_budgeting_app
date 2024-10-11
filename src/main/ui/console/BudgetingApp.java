cd8eea4cf48d381b3aba218d970eddbb1167236a
Adoroteo01 <andreitot13@gmail.com>
2024-10-11 03:33:58 -0700

    implemented budget viewer window

--- src/main/ui/console/BudgetingApp.java
+++ src/main/ui/console/BudgetingApp.java
@@ -3,8 +3,9 @@ package ui.console;
 import java.util.ArrayList;
 import java.util.List;
 
-import model.Budgeter;
+import model.Budget;
 import ui.console.menus.BudgetOpener;
+import ui.console.menus.BudgetViewer;
 import ui.console.menus.MainMenu;
 import ui.console.menus.NewBudgetMaker;
 import ui.console.menus.Window;
@@ -19,42 +20,92 @@ public class BudgetingApp {
     private Window mainMenu;
     private Window newBudgetMaker;
     private Window budgetOpener;
+    private BudgetViewer budgetViewer;
 
-    private List<Budgeter> budgeters;
+    private List<Budget> budgets;
 
     public BudgetingApp() {
         mainMenu = new MainMenu();
         newBudgetMaker = new NewBudgetMaker();
         budgetOpener = new BudgetOpener();
+        budgetViewer = new BudgetViewer();
 
-        budgeters = new ArrayList<Budgeter>();
+        budgets = new ArrayList<Budget>();
         run();
     }
 
     // EFFECTS: runs the budgeting app
     private void run() {
+        mainMenu();
+    }
+
+    // EFFECTS: Prints main menu to console
+    private void mainMenu() {
         mainMenu.open();
         userInput = mainMenu.getUserInput();
 
         switch (userInput) {
             case "1": // Open Budget
 
-                budgetOpener.open();
+                budgetOpener();
 
             case "2": // New Budget
 
-                newBudgetMaker.open();
-                userInputs = newBudgetMaker.getAllInputs();
-                makeNewBudget(userInputs.get(0), userInputs.get(1), userInputs.get(2));
-
+                newBugetMaker();
+                
             default:
-                break;
+                run();
         }
     }
 
-    // TODO
-    public void makeNewBudget(String name, String startDate, String endDate) {
-        // stub
+    private void budgetOpener() {
+        budgetOpener.open();
+        // TODO
+    }
+
+    private void newBugetMaker() {
+        newBudgetMaker.open();
+        userInputs = newBudgetMaker.getAllInputs();
+        Budget newBudget = makeNewBudget(userInputs.get(0), userInputs.get(1), userInputs.get(2));
+        budgets.add(newBudget);
+
+        bugetViewer(newBudget);
+    }
+
+    private void bugetViewer(Budget newBudget) {
+        budgetViewer.setBudget(newBudget);
+        budgetViewer.open();
+
+        userInput = budgetViewer.getUserInput();
+
+        switch (userInput) {
+            case "1": // TODO: Edit Budget Entries
+                
+                break;
+            case "2": // TODO: Track
+                
+                break;
+            case "3": // TODO: Summary
+                
+                break;
+            case "4": // TODO: Main Menu
+                
+                mainMenu();
+        
+            default:
+                mainMenu();
+        }
+    }
+
+    // MODIFIES: budgets
+    // EFFECTS: adds a new Budget to budgets and returns that new Budget
+    public Budget makeNewBudget(String name, String startDate, String endDate) {
+        Budget newBudget;
+        newBudget = new Budget(name, startDate, endDate);
+
+        budgets.add(newBudget);
+
+        return newBudget;
     }
 
 }
