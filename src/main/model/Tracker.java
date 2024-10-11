af5cc0d3e1726cd8cfe9355d93856b0f161a85f6
Adoroteo01 <andreitot13@gmail.com>
2024-10-10 19:59:45 -0700

    added tests for Tracker class

--- src/main/model/Tracker.java
+++ src/main/model/Tracker.java
@@ -1,16 +1,21 @@
 package model;
 
+import java.util.ArrayList;
 import java.util.List;
 
-// A spending tracker that holds TrackerEntrys for it's
+// A spending tracker that holds TrackerEntries for it's
 // corrisponding Budgeter
 public class Tracker {
 
-    List<TrackerEntry> entries;
+    private List<TrackerEntry> entries;
 
     // creates a new Tracker with no entries in list
     public Tracker() {
+        entries = new ArrayList<TrackerEntry>();
+    }
 
+    public List<TrackerEntry> getEntries() {
+        return entries;
     }
 
     // add entry to entries. 
