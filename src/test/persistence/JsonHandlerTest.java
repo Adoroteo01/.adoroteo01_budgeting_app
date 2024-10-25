6b64d15ebf88f68df91dd2f1ba0264859f9bed6f
Adoroteo01 <andreitot13@gmail.com>
2024-10-25 00:11:39 -0700

    added tests for budgetEntryToJson method in JsonHandler class

--- src/test/persistence/JsonHandlerTest.java
+++ src/test/persistence/JsonHandlerTest.java
@@ -7,19 +7,23 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import model.Budget;
+import model.budgetentries.BudgetEntry;
+import model.budgetentries.Expense;
 
 public class JsonHandlerTest {
     Budget b1;
+    BudgetEntry be1;
     JsonHandler jh;
 
     @BeforeEach
     void setup() {
         b1 = new Budget("B1", "Oct 1", "Nov 1");
+        be1 = new Expense("1000", "BE1", 200);
         jh = new JsonHandler();
     }
 
     @Test
-    void testbudgetPrimitiveFieldsToJson() {
+    void testBudgetPrimitiveFieldsToJson() {
         JSONObject jsonObject = jh.budgetPrimitiveFieldsToJson(b1);
         String jsonName = jsonObject.optString("name");
         String jsonStartDate = jsonObject.optString("startDate");
@@ -30,4 +34,21 @@ public class JsonHandlerTest {
         assertEquals(b1.getEndDate(), jsonEndDate);
 
     }
+
+    @Test
+    void testBudgetEntryToJson() {
+        JSONObject jsonObject = jh.budgetEntryToJson(be1);
+        String jsonId = jsonObject.optString("id");
+        String jsonName = jsonObject.optString("name");
+        Double jsonBudgetAmount = jsonObject.optDouble("budgetAmount");
+        Double jsonActualAmount = jsonObject.optDouble("actualAmount");
+
+        assertEquals(be1.getId(), jsonId);
+        assertEquals(be1.getName(), jsonName);
+        assertEquals(be1.getBudgetAmount(), jsonBudgetAmount);
+        assertEquals(be1.getActualAmount(), jsonActualAmount);
+
+    }
+
+
 }
