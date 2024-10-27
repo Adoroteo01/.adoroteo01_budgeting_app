e3b2696521d075b4136ce28898b1dfe4cdb7857b
Adoroteo01 <andreitot13@gmail.com>
2024-10-27 11:00:13 -0700

    added tests for budgetEntriesToJson method in JsonHandler class

--- src/test/persistence/JsonHandlerTest.java
+++ src/test/persistence/JsonHandlerTest.java
@@ -2,7 +2,11 @@ package persistence;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.json.JSONObject;
+import org.json.JSONArray;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -13,21 +17,29 @@ import model.budgetentries.Expense;
 public class JsonHandlerTest {
     Budget b1;
     BudgetEntry be1;
+    BudgetEntry be2;
+    List<BudgetEntry> listOfBE;
     JsonHandler jh;
 
     @BeforeEach
     void setup() {
         b1 = new Budget("B1", "Oct 1", "Nov 1");
         be1 = new Expense("1000", "BE1", 200);
+        be2 = new Expense("1001", "BE2", 500);
+        
+        listOfBE = new ArrayList<BudgetEntry>();
+        listOfBE.add(be1);
+        listOfBE.add(be2);
+        
         jh = new JsonHandler();
     }
 
     @Test
     void testBudgetPrimitiveFieldsToJson() {
-        JSONObject jsonObject = jh.budgetPrimitiveFieldsToJson(b1);
-        String jsonName = jsonObject.optString("name");
-        String jsonStartDate = jsonObject.optString("startDate");
-        String jsonEndDate = jsonObject.optString("endDate");
+        JSONObject jsonPrimitiveFields = jh.budgetPrimitiveFieldsToJson(b1);
+        String jsonName = jsonPrimitiveFields.optString("name");
+        String jsonStartDate = jsonPrimitiveFields.optString("startDate");
+        String jsonEndDate = jsonPrimitiveFields.optString("endDate");
 
         assertEquals(b1.getName(), jsonName);
         assertEquals(b1.getStartDate(), jsonStartDate);
@@ -37,16 +49,44 @@ public class JsonHandlerTest {
 
     @Test
     void testBudgetEntryToJson() {
-        JSONObject jsonObject = jh.budgetEntryToJson(be1);
-        String jsonId = jsonObject.optString("id");
-        String jsonName = jsonObject.optString("name");
-        Double jsonBudgetAmount = jsonObject.optDouble("budgetAmount");
-        Double jsonActualAmount = jsonObject.optDouble("actualAmount");
+        JSONObject jsonBudgetEntry = jh.budgetEntryToJson(be1);
+        String jsonId = jsonBudgetEntry.optString("id");
+        String jsonName = jsonBudgetEntry.optString("name");
+        Double jsonBudgetAmount = jsonBudgetEntry.optDouble("budgetAmount");
+        Double jsonActualAmount = jsonBudgetEntry.optDouble("actualAmount");
 
         assertEquals(be1.getId(), jsonId);
         assertEquals(be1.getName(), jsonName);
         assertEquals(be1.getBudgetAmount(), jsonBudgetAmount);
         assertEquals(be1.getActualAmount(), jsonActualAmount);
+
+    }
+
+    @Test
+    void testBudgetEntriesToJson() {
+        JSONArray jsonBudgetEntries = jh.budgetEntriesToJson(listOfBE);
+        JSONObject jsonBudgetEntry1 = jsonBudgetEntries.optJSONObject(0);
+        JSONObject jsonBudgetEntry2 = jsonBudgetEntries.optJSONObject(1);
+        
+        String jsonId1 = jsonBudgetEntry1.optString("id");
+        String jsonName1 = jsonBudgetEntry1.optString("name");
+        Double jsonBudgetAmount1 = jsonBudgetEntry1.optDouble("budgetAmount");
+        Double jsonActualAmount1 = jsonBudgetEntry1.optDouble("actualAmount");
+
+        assertEquals(be1.getId(), jsonId1);
+        assertEquals(be1.getName(), jsonName1);
+        assertEquals(be1.getBudgetAmount(), jsonBudgetAmount1);
+        assertEquals(be1.getActualAmount(), jsonActualAmount1);
+
+        String jsonId2 = jsonBudgetEntry2.optString("id");
+        String jsonName2 = jsonBudgetEntry2.optString("name");
+        Double jsonBudgetAmount2 = jsonBudgetEntry2.optDouble("budgetAmount");
+        Double jsonActualAmount2 = jsonBudgetEntry2.optDouble("actualAmount");
+
+        assertEquals(be2.getId(), jsonId2);
+        assertEquals(be2.getName(), jsonName2);
+        assertEquals(be2.getBudgetAmount(), jsonBudgetAmount2);
+        assertEquals(be2.getActualAmount(), jsonActualAmount2);
 
     }
 
