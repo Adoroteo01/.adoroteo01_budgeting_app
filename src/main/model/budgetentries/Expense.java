d7fc07b19f629a0b53407e07aabb90abb157c842
Adoroteo01 <andreitot13@gmail.com>
2024-10-30 17:16:31 -0700

    edited specification for Expense class

--- src/main/model/budgetentries/Expense.java
+++ src/main/model/budgetentries/Expense.java
@@ -3,10 +3,12 @@ package model.budgetentries;
 // A budget entry of an expense that has a name, budgeted amount, and actual amount
 public class Expense extends BudgetEntry {
 
+    // EFFECT: creates Expense with given id, name, and budgetAmount
     public Expense(String id, String name, double budgetAmount) {
         super(id, name, budgetAmount);
     }
 
+    // EFFECT: creates Expense with given id, name, budgetAmount, and actualAmount
     Expense(String id, String name, double budgetAmount, double actualAmount) {
         super(id, name, budgetAmount, actualAmount);
     }
