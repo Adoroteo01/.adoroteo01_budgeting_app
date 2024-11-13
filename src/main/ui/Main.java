dc5772d7f06bdc27ba414f570304423b9f3ad027
Adoroteo01 <andreitot13@gmail.com>
2024-11-12 19:25:56 -0800

    added new package and specifications for gui

--- src/main/ui/Main.java
+++ src/main/ui/Main.java
@@ -1,10 +1,24 @@
 package ui;
 
-import ui.console.BudgetingApp;
+import javax.swing.UIManager;
+
+// import java.awt.*;
+import com.formdev.flatlaf.FlatLightLaf;
+
+import ui.gui.GraphicalBudgetingApp;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
-        
-        new BudgetingApp();
+    public static void main(String[] args) {
+
+        // Sets the theme of Swing UI
+        FlatLightLaf.setup();
+        try {
+            UIManager.setLookAndFeel(new FlatLightLaf());
+        } catch (Exception e) {
+            System.out.println("Look and Feel Loading Error");
+        }
+
+        // new BudgetingApp(); // for console ui
+        new GraphicalBudgetingApp(); // for gui
     }
 }
---
+++ src/main/ui/gui/GraphicalBudgetingApp.java
@@ -0,0 +1,14 @@
+package ui.gui;
+
+public class GraphicalBudgetingApp {
+    
+    // Creates new Budgeting app
+    public GraphicalBudgetingApp() {
+        run();
+    }
+
+    // EFFECTS: Runs Budgeting app gui
+    private void run() {
+        
+    }
+}
