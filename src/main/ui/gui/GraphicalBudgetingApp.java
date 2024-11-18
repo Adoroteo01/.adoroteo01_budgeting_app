40e407224ae21494dac2f16f3c64d7e9f66f1c9a
Adoroteo01 <andreitot13@gmail.com>
2024-11-17 20:46:17 -0800

    added specifications and implemented main app window

--- src/main/ui/gui/GraphicalBudgetingApp.java
+++ src/main/ui/gui/GraphicalBudgetingApp.java
@@ -1,12 +1,19 @@
 package ui.gui;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Component;
 import java.awt.Dimension;
+import java.awt.Font;
 
+import javax.swing.Box;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.border.Border;
+import javax.swing.border.EmptyBorder;
 
 public class GraphicalBudgetingApp {
 
@@ -25,18 +32,63 @@ public class GraphicalBudgetingApp {
         createAppWindow();
 
         JPanel sideMenu = createSideMenu();
-
-        JPanel mainContent = createMainContentPanel();
+        JPanel content = createContentPanel();
 
         appWindow.add(sideMenu, BorderLayout.WEST);
-        appWindow.add(mainContent, BorderLayout.EAST);
+        appWindow.add(content, BorderLayout.CENTER);
 
         appWindow.setVisible(true);
     }
 
-    // EFFECT: returns Jpanel for Main functionality of the app
+    // EFFECT: returns Jpanel for app content with padding on the left
+    private JPanel createContentPanel() {
+
+        JPanel content = new JPanel();
+        content.setLayout(new BorderLayout());
+
+        JPanel padding = createPadding(0, 55, 0, 0);
+
+        // Main Content
+        JPanel mainContent = createMainContentPanel();
+
+        // Adding compenents
+        content.add(padding, BorderLayout.WEST);
+        content.add(mainContent, BorderLayout.CENTER);
+        return content;
+    }
+
+    // EFFECT: returns empty JPanel that has top space at the top, left space on the
+    // left, bottom space at the bottom, and right space on the right
+    private JPanel createPadding(int top, int left, int bottom, int right) {
+        // Padding
+        JPanel padding = new JPanel();
+        padding.setBorder(new EmptyBorder(top, left, bottom, right));
+        padding.setBackground(Color.RED); // TODO: Delete later. For testing
+        return padding;
+    }
+
+    // EFFECT: returns JPanel for the main content of the app such as adding
+    // budgets, budget entries, tracker entries, etc.
     private JPanel createMainContentPanel() {
         JPanel mainContent = new JPanel();
+        mainContent.setLayout(new BorderLayout());
+
+        // Top Logo bar
+        JPanel titleBar = new JPanel();
+        titleBar.setLayout(new BoxLayout(titleBar, BoxLayout.Y_AXIS));
+
+        JLabel titleBarLabel = new JLabel("Budget.ly");
+        titleBarLabel.setFont(new Font("Arial", Font.PLAIN, 16));
+        titleBarLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+        titleBar.add(titleBarLabel);
+
+        // Test content
+        JButton testButton = new JButton("TEST BUTTON!");
+
+        mainContent.add(titleBar, BorderLayout.NORTH);
+        mainContent.add(testButton, BorderLayout.CENTER);
+
         return mainContent;
     }
 
